d6dec562011865f79fa447e284e3d35f279de85d
“zwq2017040393git config --global user.name “zwq2017040393 <yozwq12899847@qq.com>
2021-01-07 15:42:20 +0800

    update

--- app/src/main/java/baidumapsdk/demo/layers/LayersList.java
+++ app/src/main/java/baidumapsdk/demo/layers/LayersList.java
@@ -1,4 +1,4 @@
-package baidumapsdk.demo.createmap;
+package baidumapsdk.demo.layers;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,12 +8,10 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 
 import baidumapsdk.demo.R;
-import baidumapsdk.demo.layers.LayerBuilding;
-import baidumapsdk.demo.layers.LayerTrafficAndHeatMap;
 import baidumapsdk.demo.util.DemoInfo;
 import baidumapsdk.demo.util.DemoListAdapter;
 
-public class CreateMapList extends AppCompatActivity {
+public class LayersList extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +19,7 @@ public class CreateMapList extends AppCompatActivity {
         setContentView(R.layout.activity_menulist);
         ListView demoList = (ListView) findViewById(R.id.mapList);
         // 添加ListItem，设置事件响应
-        demoList.setAdapter(new DemoListAdapter(CreateMapList.this,DEMOS));
+        demoList.setAdapter(new DemoListAdapter(LayersList.this,DEMOS));
         demoList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> arg0, View v, int index, long arg3) {
                 onListItemClick(index);
@@ -31,18 +29,14 @@ public class CreateMapList extends AppCompatActivity {
 
     void onListItemClick(int index) {
         Intent intent;
-        intent = new Intent(CreateMapList.this, DEMOS[index].demoClass);
+        intent = new Intent(this, DEMOS[index].demoClass);
         this.startActivity(intent);
     }
 
     private static final DemoInfo[] DEMOS = {
-            new DemoInfo(R.string.demo_title_map_type, R.string.demo_desc_map_type, MapType.class),
-            new DemoInfo(R.string.demo_title_custommap, R.string.demo_desc_custommap, CustomMap.class),
             new DemoInfo(R.string.demo_title_layertraffic, R.string.demo_desc_layertraffic, LayerTrafficAndHeatMap.class),
             new DemoInfo(R.string.demo_title_layerbuilding, R.string.demo_desc_layerbuilding, LayerBuilding.class),
-            new DemoInfo(R.string.demo_title_indoor, R.string.demo_desc_indoor, IndoorMap.class),
-            new DemoInfo(R.string.demo_title_multimapview, R.string.demo_desc_multimapview, MultiMapView.class),
-            new DemoInfo(R.string.demo_title_offline, R.string.demo_desc_offline, Offline.class)
+            new DemoInfo(R.string.demo_title_locationtype, R.string.demo_desc_locationtype, LocationTypeDemo.class),
+            new DemoInfo(R.string.demo_title_locationcustom, R.string.demo_desc_locationcustom, LocationCustom.class)
     };
 }
-
