bf721f0839493af2dd31a4093540474d57990ed1
coolkeyee <1057579724@qq.com>
2021-01-08 20:23:27 +0800

    2021/01/08

--- app/src/main/java/baidumapsdk/demo/layers/LayerTrafficAndHeatMap.java
+++ app/src/main/java/baidumapsdk/demo/layers/LayerTrafficAndHeatMap.java
@@ -1,20 +1,47 @@
 package baidumapsdk.demo.layers;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
 import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
 
 import baidumapsdk.demo.R;
 
-public class LayerTrafficAndHeatMap extends AppCompatActivity {
+public class LayerTrafficAndHeatMap extends AppCompatActivity implements SensorEventListener {
 
     // MapView 地图主控件
     private MapView mMapView;
     private BaiduMap mBaiduMap;
+    //定位显示
+    private LocationClient mLocClient;
+    private LayerTrafficAndHeatMap.My_Heat_LocationListener myListener = new LayerTrafficAndHeatMap.My_Heat_LocationListener();
+    // 定位图层显示方式
+    private MyLocationConfiguration.LocationMode mCurrentMode;
+    private SensorManager mSensorManager;
+    private Double lastX = 0.0;
+    private int mCurrentDirection = 0;
+    private double mCurrentLat = 0.0;
+    private double mCurrentLon = 0.0;
+    private float mCurrentAccracy;
+    // 是否首次定位
+    private boolean isFirstLoc = true;
+    //定位信息
+    private MyLocationData myLocationData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +49,30 @@ public class LayerTrafficAndHeatMap extends AppCompatActivity {
         setContentView(R.layout.activity_layers);
         mMapView = (MapView) findViewById(R.id.bmapView);
         mBaiduMap = mMapView.getMap();
+
+
+    }
+
+    //传感器接口如下
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {  //传感器的值发生变化
+        double x = sensorEvent.values[SensorManager.DATA_X];
+        if (Math.abs(x - lastX) > 1.0) {
+            mCurrentDirection = (int) x;
+            myLocationData = new MyLocationData.Builder()
+                    .accuracy(mCurrentAccracy)// 设置定位数据的精度信息，单位：米
+                    .direction(mCurrentDirection)// 此处设置开发者获取到的方向信息，顺时针0-360
+                    .latitude(mCurrentLat)
+                    .longitude(mCurrentLon)
+                    .build();
+            mBaiduMap.setMyLocationData(myLocationData);
+        }
+        lastX = x;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {  //传感器的精度发生变化
+
     }
 
     /**
@@ -36,6 +87,35 @@ public class LayerTrafficAndHeatMap extends AppCompatActivity {
      */
     public void setHeatMap(View view) {
         mBaiduMap.setBaiduHeatMapEnabled(((CheckBox) view).isChecked());
+    }
+    /**
+     * 定位SDK监听函数
+     */
+    public class My_Heat_LocationListener implements BDLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            // MapView 销毁后不在处理新接收的位置
+            if (location == null || mMapView == null) {
+                return;
+            }
+            mCurrentLat = location.getLatitude();
+            mCurrentLon = location.getLongitude();
+            mCurrentAccracy = location.getRadius();
+            myLocationData = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())// 设置定位数据的精度信息，单位：米
+                    .direction(mCurrentDirection)// 此处设置开发者获取到的方向信息，顺时针0-360
+                    .latitude(location.getLatitude())
+                    .longitude(location.getLongitude())
+                    .build();
+            mBaiduMap.setMyLocationData(myLocationData);
+            if (isFirstLoc) {
+                isFirstLoc = false;
+                LatLng ll = new LatLng(location.getLatitude(), location.getLongitude());
+                MapStatus.Builder builder = new MapStatus.Builder();
+                builder.target(ll).zoom(18.0f);
+                mBaiduMap.animateMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()));
+            }
+        }
     }
 
     @Override
